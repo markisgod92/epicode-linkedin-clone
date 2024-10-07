7ab8ca03413b8a415addd6e0c77603bed15f2a49
giuseppe-30 <giuseppeborrino94@gmail.com>
2024-10-07 22:09:12 +0200

    footer-Day-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Footer from "./components/basic-layout/right-sidebar-footer-block/Footer";
 
 export const App = () => {
   return (
     <>
-
+      <Footer />
     </>
-  )
-}
+  );
+};
