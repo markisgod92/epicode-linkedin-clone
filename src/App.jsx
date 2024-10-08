6bb6c3f0f51d6a7937081b117a4a5fec0d363d59
Danilo323i <167352102+Danilo323i@users.noreply.github.com>
2024-10-08 19:56:22 +0200

    post-block

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import PostBox from "../src/components/DevelopCreateNewPostBlock/DevelopCreateNewPostBlock";
 
 export const App = () => {
   return (
     <>
-
+      <PostBox />
     </>
-  )
-}
+  );
+};
+
+export default App;
