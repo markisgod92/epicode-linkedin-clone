d8a512ff4dc91a821b503ba905dbb82f00f15019
GiuseppeRaso94 <peppone2894@gmail.com>
2024-10-08 20:39:55 +0200

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import LeftSideBarMainBlock from "./components/LeftSideBarMainBlock/LeftSideBarMainBlock";
 
 export const App = () => {
-  return (
-    <>
-
-    </>
-  )
-}
+  return <LeftSideBarMainBlock />;
+};
