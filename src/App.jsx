0e05806c574c6aaf21162b12df23fe563db8e57c
uros milenkovic <urosm4471@gmail.com>
2024-10-08 19:41:47 +0200

    component news

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
+//import RightSide from "../src/components/RightSideNews/RightSide";
+import RightSideNews from "./components/RightSideNews/RightSide.jsx";
 
 export const App = () => {
   return (
     <>
-
+      <RightSideNews />
     </>
-  )
-}
+  );
+};
