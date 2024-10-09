ae14a63f2c43510844e17c1a9968b2775cbac3dc
tantoquantoanto <antoniettaandreozzi91@gmail.com>
2024-10-09 13:48:06 +0200

    create post modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import { Button } from 'react-bootstrap'
+import CreatePostModal from './components/create-post-modal/CreatePostModal'
+import PostModalButton from './components/create-post-modal/PostModalButton'
 
 export const App = () => {
-  return (
-    <>
 
-    </>
+  
+
+  return (
+    <PostModalButton/>
   )
 }
---
+++ src/components/create-post-modal/PostModalButton.jsx
@@ -0,0 +1,24 @@
+import { Button } from "react-bootstrap";
+import CreatePostModal from "./CreatePostModal";
+import { useState } from "react";
+
+const PostModalButton = () => {
+    const [showModal, setShowModal] = useState(false);
+  
+ const toggleShowModal = () => setShowModal(!showModal)
+return (
+    <>
+    
+<Button
+onClick={toggleShowModal}>
+  Apri il modale
+</Button>
+<CreatePostModal
+showModal={showModal}
+toggleShowModal = {toggleShowModal}
+setShowModal={setShowModal}/>
+    </>
+)
+    
+}
+export default PostModalButton
