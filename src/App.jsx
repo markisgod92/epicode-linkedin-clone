651c8788b2c6ae3a4508770041392537e442f25c
EmilianoCastelletti <emilcast001@gmail.com>
2024-10-09 19:06:55 +0200

    left sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
 import './App.css'
+import LeftSidebar  from '/Users/emilc/Desktop/buildweek 3/epicode-linkedin-clone/src/components/basic-layout/leftsidebar/LeftSidebar.jsx'
 
 export const App = () => {
   return (
     <>
-
     </>
-  )
-}
+    )
