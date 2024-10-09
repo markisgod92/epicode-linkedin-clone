f49cdafb808f0d7ce9a93bf2ee757d7b428f0d2e
uros milenkovic <urosm4471@gmail.com>
2024-10-09 17:35:06 +0200

    Develop right sidebar news block

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
-import './App.css'
-import { BasicLayout } from './components/basic-layout/BasicLayout'
+import "./App.css";
+import { BasicLayout } from "./components/basic-layout/BasicLayout";
 import { SinglePost } from "./components/single-post/SinglePost";
 import Navigation from "./components/navigation/Navigation";
 import RightSideNews from "./components/RightSideNews/RightSide.jsx";
@@ -10,11 +9,11 @@ export const App = () => {
   return (
     <>
       <RightSideNews />
-    <BasicLayout />
-    <SinglePost />
-     <Navigation />
-    <RightSideNews />
-    <Footer />
+      <BasicLayout />
+      <SinglePost />
+      <Navigation />
+      <RightSideNews />
+      <Footer />
     </>
   );
 };
